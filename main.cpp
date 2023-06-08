a9e2801cbee53c37f521798957ca25536018c927
Wu <wu.vinci00@gmail.com>
2023-06-08 19:11:36 -0400

    Created user inputs to determine if they have an existing account

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <string>
 #include "Account.h"
 using namespace std;
 
 int main() {
-    Account newAccount("New", 1234, 10);
-    newAccount.getPin();
+    string accountExist;
+    do {
+        cout << "Existing member? (yes/no): ";
+        cin >> accountExist;
+    } while (accountExist != "yes" &&  accountExist != "no");
+
+    // Account exists
+    if (accountExist == "yes") {
+        cout << "Welcome back" << endl;
+    } else {
+        Account newAccount("New", 1234, 10);
+        newAccount.getPin();
+    }
     return 0;
 }
